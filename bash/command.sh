c3b9111fbec2181656f8901ca6a21429ef2c581b
prashantK <prashant.kumar@cradlepoint.com>
2024-02-25 08:14:12 +0530

    stable functioning

--- bash/command.sh
+++ bash/command.sh
@@ -1,3 +1,4 @@
 alias postman="/vo/wo/bin/framework/postman/app/postman"
 alias postgres-local="psql postgresql://rome:rome@10.5.0.6:5432/postgres?sslmode=disable"
 alias netvis-k="docker exec -it network-visibility_kafka_1 bash -c 'cd netvis/kafka; HISTFILE=/home/appuser/netvis/kafka/hist bash --rcfile rcfile'"
+alias netvis-k-="docker exec -it network-visibility-kafka-1 bash -c 'cd netvis/kafka; HISTFILE=/home/appuser/netvis/kafka/hist bash --rcfile rcfile'"
